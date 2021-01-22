c809d0e218d539e91ccae4b0d271ccb95a8fc5dc
VARAD SRIVASTAVA <varad.srivastava26@gmail.com>
2021-01-23 02:26:28 +0530

    added web app

---
+++ webapp/static/covid_classes.js
@@ -0,0 +1,5 @@
+const COVID_CLASSES = {
+    0: 'Covid-19 probability ',
+    1: 'Normal probability ',
+    2: 'Viral Pneuomina probability'
+};
---
+++ webapp/static/covid_classes_ct.js
@@ -0,0 +1,4 @@
+const COVID_CLASSES = {
+    0: 'Covid-19 probability ',
+    1: 'Non-Covid-19 probability ',
+};
